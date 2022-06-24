e59154e97b3acab1ce0e0b78e3981d5842450466
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-06-24 08:53:34 +0000

    Add functions to fetch data from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './style.css';
+import './style.css'; 
+import { addNewScore, getAllScores } from './modules/updateScores';
+
+const refreshButton = document.querySelector('#refresh');
+const addButton = document.querySelector('#addButton');
+
+getAllScores();
+
+addButton.addEventListener('click', (e) => {
+  addNewScore(); 
+})
+
+refreshButton.addEventListener('click', (e) => {
+  getAllScores(); 
+})
